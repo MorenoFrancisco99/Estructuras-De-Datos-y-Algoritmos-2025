b6abacb25d21ab677bcc8058d14e61ea7d109f50
Francisco Moreno <franciscomjjp@gmail.com>
2025-10-27 17:10:08 -0300

    TestCommit

--- Practico 6/TADs/GrafoSecuencial.cs
+++ Practico 6/TADs/GrafoSecuencial.cs
@@ -8,5 +8,17 @@ namespace ConsoleApp1.Practico_6
 {
     internal class GrafoSecuencial
     {
+        private int[] grafo;
+
+
+        public GrafoSecuencial(int cantidadDeVertices)
+        {
+            grafo = new int[cantidadDeVertices * cantidadDeVertices];
+            for (int i = 0; i < grafo.Length; i++)
+            {
+                grafo[i] = 0;
+            }
+        }
+
     }
 }
